71592ffdca2090de1cd26ca271c5bffdb7fd7a4e
artem <13_22_54@mail.ru>
2023-12-22 19:58:13 +0700

    Enable ctest from build dir

--- tests/test-cases/HelloWorldTest.cpp
+++ tests/test-cases/HelloWorldTest.cpp
@@ -5,3 +5,11 @@
 TEST(HelloWorldBaseTests, testEqual){
     ASSERT_EQ(TemporarySpace::getHelloWorldString(), "Hello my World!");
 }
+
+TEST(HelloWorldBaseTests, testEqual2){
+    EXPECT_EQ(TemporarySpace::getHelloWorldString(), "Hello my World!");
+}
+
+TEST(HelloWorldBaseTests, testEqual3){
+    ASSERT_EQ(0,0);
+}
